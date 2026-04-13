f4c36eface94ce1bc5891a2cc388ac7145961fc1
jcproaudiopro-blip <jcproaudiopro@gmail.com>
2026-04-13 19:12:03 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,3 @@
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('radio-cache').then(cache => {
-      return cache.addAll(['/', '/index.html']);
-    })
-  );
+  self.skipWaiting();
 });
